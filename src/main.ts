979456a6473a42296bae1678cd8f283fa5415783
Joey Quadros <joey_qpb@hotmail.com>
2023-02-15 18:37:36 -0300

    feat: add cross comentado

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { LogInterceptor } from './interceptors/log.interceptor';
+// import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // app.enableCors({
+  //   methods: ['GET'],
+  //   origin: [],
+  // });
+
   app.useGlobalPipes(new ValidationPipe());
+
   // app.useGlobalInterceptors(new LogInterceptor());
+
   await app.listen(Number(process.env.PORT));
+
   console.log(`server up and running at port ${process.env.PORT}!`);
 }
 bootstrap();
